8a9f6a456534c9719a476ce5a144e68cceedd54f
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-05-12 21:05:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
+cd /$REPONAME
+cp -r * /
 cd /
+command="python3 /app.py"
+log="/data.txt"
+match="WORD"
+
+$command > "$log" 2>&1 &
+pid=$!
+
+while sleep 60
+do
+    if fgrep --quiet "$match" "$log"
+    then
+        pkill python3
+        bash /entrypoint.sh
+        rm data.txt
+        exit 0
+    fi
+done
