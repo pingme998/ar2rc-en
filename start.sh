61a3da90ff3aad9dbf1109b19b183857dd3f95fe
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-05-13 18:44:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-jupyter notebook --ip=0.0.0.0 --port=$PORT --NotebookApp.token='' --NotebookApp.password=''
+# jupyter notebook --ip=0.0.0.0 --port=$PORT --NotebookApp.token='' --NotebookApp.password=''
 cd /$REPONAME
 cp -r * /
 cat /$REPONAME/app.py |sed "s/THEPORTNUMBER/$PORT/g" >/app.py
