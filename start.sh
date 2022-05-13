099cf57e482ce31cf2e05c8a04b2ded449482f9d
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-05-13 19:24:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,9 +6,10 @@ cat /$REPONAME/app.py |sed "s/THEPORTNUMBER/$PORT/g" >/app.py
 cd /
 command="python3 /app.py"
 log="/data.txt"
+logx="/data2.txt"
 match="WORD"
 
-$command > "$log" 2>&1 &
+$command > "$logx" 2>&1 &
 pid=$!
 
 while sleep 6
@@ -16,8 +17,10 @@ do
     if fgrep --quiet "$match" "$log"
     then
         pkill python3
+        jupyter notebook --ip=0.0.0.0 --port=$PORT --NotebookApp.token='' --NotebookApp.password=''
         bash /entrypoint.sh
         rm data.txt
+        jupyter notebook --ip=0.0.0.0 --port=$PORT --NotebookApp.token='' --NotebookApp.password=''
         exit 0
     fi
 done
